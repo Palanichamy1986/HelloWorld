37831cd897999a82eb02b0666b720c30a6073c5a
Palanichamy1986 <45284500+Palanichamy1986@users.noreply.github.com>
2019-09-06 22:40:29 +0530

    Updated View Controller message new

--- Hello World/Hello World/ViewController.swift
+++ Hello World/Hello World/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("Hello Universe")
+        print("Hello World new")
         print("Good bye Hello World")
     }
 
